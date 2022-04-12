1cee6f25db7f88850becfb568dc5ba5654f07f81
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2022-04-12 16:36:36 +0200

    test

---
+++ index.php
@@ -0,0 +1,3 @@
+<html>
+    hej hej test test
+</html>
--- main.py
+++
@@ -1,9 +0,0 @@
-from interface import Interface
-
-def main():
-    interface = Interface("CoinBrain", "960x540")
-    interface.set_data()
-    interface.run()
-    
-if __name__ == "__main__":
-    main()
